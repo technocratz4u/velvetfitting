061d78d3e34a6bfdbaf1b2059be616f554e35afe
Manikandan Rajaraman <ju.manikandan@gmail.com>
2016-05-13 10:12:47 +0530

    Installed - Open Sans and Josefin Slab, Added style for logo image

--- views/header.php
+++ views/header.php
@@ -1,5 +1,5 @@
 <div class="brand">
-		<img alt="Logo" src="static/img/logo.gif">Velvet Fitting
+		<img alt="Logo" src="static/img/logo.gif" id="logo-img" />Velvet Fitting
 	</div>
 	<div class="address-bar">Passionate Bathing...</div>
 
@@ -21,7 +21,7 @@
 			<div class="collapse navbar-collapse"
 				id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
-					<li><a href="<?php echo __WEB_ROOT?>/#">Home</a></li>
+					<li><a href="<?php echo __WEB_ROOT?>">Home</a></li>
 					<li><a href="<?php echo __WEB_ROOT?>/product">Products</a></li>
 					<li><a href="<?php echo __WEB_ROOT?>/about">About</a></li>
 					<li><a href="<?php echo __WEB_ROOT?>/contact">Contact</a></li>
