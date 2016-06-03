bbc3c59fd2a29d02052b1e7e035f9fa0df2c9920
Manikandan Rajaraman <ju.manikandan@gmail.com>
2016-06-03 15:10:47 +0530

    Added noindex meta tag to stop from being indexed

--- views/inquire.php
+++ views/inquire.php
@@ -6,6 +6,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="">
 <meta name="author" content="">
+<meta name="robots" content="noindex">
 
 <title>Contact Velvet Fitting - PRIME CREATIONS</title>
 
