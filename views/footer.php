686b94a271718dbc7b0c5515360b90ca20f6c7b4
Manikandan Rajaraman <ju.manikandan@gmail.com>
2016-06-09 12:45:32 +0530

    Corrected footer style for responsiveness

--- views/footer.php
+++ views/footer.php
@@ -1,7 +1,7 @@
 <footer>
 	<div class="container">
 		<div class="row" id="footer-info-row">
-			<div class="col-xs-3">
+			<div class="col-md-3 col-sm-6 col-xs-12">
 				<h4>Pages</h4>
 				<a href="<?php echo __APPLICATION_URL?>">Home</a><br> 
 				<a href="<?php echo __WEB_ROOT?>/category">Products</a><br> 
@@ -9,19 +9,24 @@
 				<a href="<?php echo __WEB_ROOT?>/contact">Contact us</a><br> 
 				<a href="#">Site Map</a><br>
 			</div>
-
-			<div class="col-xs-3">
+			<div class="clearfix visible-xs-block"></div>
+			<hr class="visible-xs-block footer-separator" />
+			<div class="col-md-3 col-sm-6 col-xs-12">
 				<h4>Where to find us</h4>
 				<strong>PRIME CREATIONS</strong><br>B-23, DSIDC Compound <br>Jhilmil Industrial Area
 				<br>Shahdara, Delhi - 110095 <br>India
 			</div>
-			<div class="col-xs-3">
+			<div class="clearfix visible-xs-block visible-sm-block"></div>
+			<hr class="visible-xs-block visible-sm-block footer-separator" />
+			<div class="col-md-3 col-sm-6 col-xs-12">
 				<h4>Contact Us</h4>
 				<i class="fa fa-user fa-fw"></i>Key Personnel: <br>Mr. Sachin Aggarwal
 				(Partner)<br> <i class="fa fa-phone fa-fw"></i>Contact:<br>
 				91-11-22135206, +919999885214
 			</div>
-			<div class="col-xs-3">
+			<div class="clearfix visible-xs-block"></div>
+			<hr class="visible-xs-block footer-separator" />
+			<div class="col-md-3 col-sm-6 col-xs-12">
 				<h4>Stay in touch</h4>
 				<p class="social">
 					<a id="footer-facebook-link" href="#"><i
@@ -30,11 +35,7 @@
 						id="footer-instagram-link" href="#"><i
 						class="fa fa-instagram fa-fw"></i></a> <a
 						id="footer-google-plus-link" href="#"><i
-						class="fa fa-google-plus fa-fw"></i></a> <a id="footer-line-link"
-						href="" target="_blank"> <img
-						src="<?php echo __WEB_ROOT?>/static/img/linebutton_30x30_en.png"
-						width="30px" height="30px" alt="LINE it!" /></a>
-
+						class="fa fa-google-plus fa-fw"></i></a> 
 				</p>
 			</div>
 		</div>
