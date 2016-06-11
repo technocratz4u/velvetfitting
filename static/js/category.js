900a9261a6526856ec499a4267ead3276672cfd5
Manikandan Rajaraman <ju.manikandan@gmail.com>
2016-06-11 13:07:46 +0530

    Changed animation of category page

--- static/js/category.js
+++ static/js/category.js
@@ -1,11 +1,28 @@
+var moduloIndex = 0;
+var intervalIndex = 4;
 $(document).ready(function() {
 	
 	$(".navigation_menu").removeClass("active");
 	$("#menu_products").addClass("active");
 	
-	$(".hot-this-week-img-container img").addClass("animated pulse");
-	
 	if($("#selectedSubCategoryId").val().length>0){
 		$("#subctgr_"+$("#selectedSubCategoryId").val()).addClass("active");
 	}
+	
+	setInterval(animateCategoryElements, 5000);
 });
+
+function animateCategoryElements(){
+	var currentIndex = moduloIndex;
+	console.log("----------------------------- moduloIndex:"+moduloIndex);
+	while(currentIndex<$(".hot-this-week-img-container img").length){
+		console.log("---:"+currentIndex);
+		var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+		$(".hot-this-week-img-container img:eq("+currentIndex+")").addClass("animated flip").one(animationEnd, function() {
+            $(this).removeClass("animated flip");
+        });
+		currentIndex += intervalIndex;
+	}
+	
+	moduloIndex = (moduloIndex+1)%intervalIndex;
+}
