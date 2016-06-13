d76884bc84ad84bcfb4b2c902417c08853fa3383
Rahul <aggarwalrahul86@gmail.com>
2016-06-13 12:50:07 +0530

    Changing About page

--- views/about.php
+++ views/about.php
@@ -4,11 +4,13 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="About Us page for Velvet Fitting - PRIME CREATIONS bathroom fitting manufacturer cum supplier, ISO certified manufacturer of classy and affordable faucet based out of Delhi, India">
+<meta name="description"
+	content="About Us page for Velvet Fitting - PRIME CREATIONS bathroom fitting manufacturer cum supplier, ISO certified manufacturer of classy and affordable faucet based out of Delhi, India">
 
 <link rel="canonical" href="<?php echo __APPLICATION_URL?>/about" />
 
-<title>Bathroom Fitting Manufacturer | Faucet Supplier | About | Velvet Fitting</title>
+<title>Bathroom Fitting Manufacturer | Faucet Supplier | About | Velvet
+	Fitting</title>
 
 <?php include "header_includes.php"; ?>
 
@@ -22,7 +24,8 @@
 
 </head>
 
-<body data-0-top-top="background-position:0px 0px;" data-0-bottom-bottom="background-position:0px -10000px;">
+<body data-0-top-top="background-position:0px 0px;"
+	data-0-bottom-bottom="background-position:0px -10000px;">
 
 	<?php include "header.php"; ?>
 
@@ -31,88 +34,100 @@
 	<div class="container" id="page-container">
 
 		<div class="row">
-			<div class="box">
-				<div class="col-lg-12">
-					<hr>
-					<h2 class="intro-text text-center">
-						About <strong>Velvet Fitting - Prime Creations</strong>
-					</h2>
-					<hr>
-				</div>
-				<div class="col-md-12">
-					<p>We are a prominent Exporter, Manufacturer, Wholesaler /
-						Distributor, Supplier, Trader, Importer of a quality approved
-						array of Bathroom Fittings &amp; Accessories. Our catered array
-						includes Bathroom Taps, Bathroom Tap Fitting, Brass Bathroom Taps,
-						Shower Heads, Shower Taps, Faucets, Shower, Basin Faucets Taps,
-						Single Hole Basin Taps and many more. These products are
-						manufactured using best quality components and employing advanced
-						technology at our manufacturing facility. Clients widely
-						appreciate the products offered by us following to their beautiful
-						design, shape, dimension and alluring look. Being fitted in
-						numerous hospital, hotel and houses, our ranges are widely
-						acclaimed in abroad countries also.</p>
+			<div class="col-xs-12">
+				<div class="box">
+					<div class="row">
+						<div class="col-xs-12">
+							<hr>
+							<h2 class="intro-text text-center">
+								About <strong>Velvet Fitting - Prime Creations</strong>
+							</h2>
+							<hr>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-12 col-md-3">
+							<p class="text-center">
+								<img class="img-responsive"
+									src="http://img.tradeindia.com/cimages/65/43469/Iso.jpg"
+									alt="ISO 9001:2008 Certified">
+						
+						</div>
+						<div class="clearfix visible-xs-block"></div>
+						<div class="col-xs-12 col-md-9">
+							<p>Prime Creations is a fast growing exporter, manufacturer,
+								wholesaler / distributor, supplier, trader, and importer of
+								Bathroom Fittings &amp; Accessories. Established in 2009, Prime
+								Creations is an ISO 9001:2008 Certified company which deals with
+								all sorts of bath products including Bathroom Taps, Bathroom Tap
+								Fitting, Brass Bathroom Taps, Shower Heads, Shower Taps,
+								Faucets, Shower, Basin Faucets Taps, Single Hole Basin Taps and
+								many more.</p>
+						</div>
 
-					<p>Business Type: Exporter, Manufacturer, Wholesaler / Distributor,
-						Supplier, Trader, Importer</p>
-					<p>Primary Competitive Advantages:</p>
-					
-					
-					<ul>
-						<li><font size="2" face="verdana,arial,helvetica,sans-serif">Products
-								of high quality</font></li>
-						<li><font size="2" face="verdana,arial,helvetica,sans-serif">Transparent
-								dealings</font></li>
-						<li><font size="2" face="verdana,arial,helvetica,sans-serif">Timely
-								deliveries</font></li>
-						<li><font size="2" face="verdana,arial,helvetica,sans-serif">Rich
-								vendor base</font></li>
-					</ul>
-					
-					<p>Year of Establishment: 2009</p>
-					<p>Product Range:</p>
-					
-					
-					<ul>
-						<li>Automatic Sensor Taps</li>
-						<li>Basin Faucets Taps</li>
-						<li>Basin Taps</li>
-						<li>Bath Mixer Taps</li>
-						<li>Bath Spouts</li>
-						<li>Bath Taps</li>
-						<li>Bathroom Accessories</li>
-						<li>Bathroom Soap Dish Set</li>
-						<li>Bathroom Soap Holder</li>
-						<li>Bathroom Tap Fitting</li>
-						<li>Bathroom Taps</li>
-						<li>Bib Cock</li>
-						<li>Bidet Taps</li>
-						<li>Brass Bathroom Taps</li>
-						<li>Center Hole Basin Mixer</li>
-						<li>Faucets</li>
-						<li>Shower</li>
-						<li>Shower Heads</li>
-						<li>Shower Taps</li>
-						<li>Single Hole Basin Taps</li>
-						<li>Single Lever Center Hole Basin Mixer</li>
-						<li>Towel Hanger</li>
-					</ul>
-					
+					</div>
+					<div class="row">
+						<div class="col-xs-12">
+							<p>The products conform to the highest quality standards and are
+								manufactured using best quality components from most reliable
+								and certified vendors of the industry, and employing advanced
+								technology at our manufacturing facility. The modern
+								infrastructure facility, which acts as a backbone of the
+								company, is armed with modern machines and tools which are
+								required for manufacturing the catered range in bulk quantity.</p>
+							<p>A highly efficient team of professionals are engaged to
+								satisfy diverse Customer needs. The products, which are made
+								available in a variety of colors, sizes and designs in order to
+								meet individual Client preference, are widely appreciated
+								because of the aesthetics and highest quality standards. Its
+								Clientele ranges from hospitals to hotels and houses worldwide.</p>
+							<hr>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-12">
+							<p>Product Range:</p>
 
-					<p>
-						<strong> &quot;We mainly deal with bulk quantity order
-							inquiries.&quot; </strong>
-					</p>
-					<p>
-						<img src="http://img.tradeindia.com/cimages/65/43469/Iso.jpg"><br>
-						<font face="Arial, Helvetica, sans-serif" size="1"><span
-							class="iso">ISO 9001:2008</span></font>
-					</p>
+							<ul>
+								<li class="col-xs-6 col-md-3">Automatic Sensor Taps</li>
+								<li class="col-xs-6 col-md-3">Basin Faucets Taps</li>
+								<li class="col-xs-6 col-md-3">Basin Taps</li>
+								<li class="col-xs-6 col-md-3">Bath Mixer Taps</li>
+								<li class="col-xs-6 col-md-3">Bath Spouts</li>
+								<li class="col-xs-6 col-md-3">Bath Taps</li>
+								<li class="col-xs-6 col-md-3">Bathroom Accessories</li>
+								<li class="col-xs-6 col-md-3">Bathroom Soap Dish Set</li>
+								<li class="col-xs-6 col-md-3">Bathroom Soap Holder</li>
+								<li class="col-xs-6 col-md-3">Bathroom Tap Fitting</li>
+								<li class="col-xs-6 col-md-3">Bathroom Taps</li>
+								<li class="col-xs-6 col-md-3">Bib Cock</li>
+								<li class="col-xs-6 col-md-3">Bidet Taps</li>
+								<li class="col-xs-6 col-md-3">Brass Bathroom Taps</li>
+								<li class="col-xs-6 col-md-3">Center Hole Basin Mixer</li>
+								<li class="col-xs-6 col-md-3">Faucets</li>
+								<li class="col-xs-6 col-md-3">Shower</li>
+								<li class="col-xs-6 col-md-3">Shower Heads</li>
+								<li class="col-xs-6 col-md-3">Shower Taps</li>
+								<li class="col-xs-6 col-md-3">Single Hole Basin Taps</li>
+								<li class="col-xs-6 col-md-3">Single Lever Center Hole Basin
+									Mixer</li>
+								<li class="col-xs-6 col-md-3">Towel Hanger</li>
+							</ul>
+						</div>
+					</div>
+					<div class="row">
+						<hr>
+						<div class="col-xs-12">
+							<p>
+								<strong> &quot;We mainly deal with bulk quantity order
+									inquiries.&quot; </strong>
+							</p>
 
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
-
 
 	</div>
 	<!-- /.container -->
